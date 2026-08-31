8c48470d1776a7616d62bbc68776c98f088f181c
math359 <cobamatheuscoba@gmail.com>
2026-08-31 09:55:50 -0300

    fazendo um cacanique(começo)

---
+++ cacanique.js
@@ -0,0 +1,18 @@
+const simbolo [
+    "maça.avif"
+    "9512451.png"
+    "103.png"
+    "356f893bebfe624018ec2b8901f13286.jpg"
+]
+
+const containerSlot1 = document.getElementById('slot1');
+const containerSlot1 = document.getElementById('slot1');
+const containerSlot1 = document.getElementById('slot1');
+const BotaGirar = document.getElementById('btnJogar');
+const elementoMensagem = document.getElementById('mensagem');
+
+BotaGirar.addEventListener('click', function(){
+    const indice1 = Math.floor (Math.random () * simbolo.lenath):
+
+
+})
